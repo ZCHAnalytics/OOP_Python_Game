474393d40e8d5dec598ec8b6a2e6b98ceb450571
Zulfia <zchanalytics@gmail.com>
2024-01-26 16:47:24 +0000

    Adds docstrings and inline comments to the hangman code

--- milestone5.py
+++ milestone5.py
@@ -1,96 +1,129 @@
 import random
-# class definition
-class HangMan: 
-    """ 
-    This class represents a game.
-    Attributes: 
-    word_list(list): list of words from which a random word is selected.
-    num_lives (int): numbers of lives or attempts that a user has before game ends.
+
+
+class Hangman: 
     """
-    # class constructor with magic method
-    def __init__(self, word_list, num_lives = 5): # TODO 2
+    A Hangman Game where a player guesses letters to reveal a mystery word.
+    
+    Parameters:
+    ----------
+        word_list (list): List of words for the game.
+        num_lives (int): Numbers of attempts allowed.
+        
+    Attributes:
+    ----------
+        self.word (str): The word to be guessed picked randomly from the word_list.
+        self.word_guessed (list): 
+            A list of the letters of the word, with '_' for each letter not yet guessed.
+            For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']
+            If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
+        self.num_letters (int): The number of UNIQUE letters in the word that have not been guessed yet.
+        self.num_lives (int): Numbers of attempts allowed. Deafult can be changed in the init method.
+        self.list_guesses (list): A list of the letters that have already been tried.
+
+    Methods:
+    -------
+        check_guess(guess): Checks if the guess which a letter provided by the user is in the word. 
+        ask_for_input(): Asks for player's input, checkes if it is a single, alphabetical character (not a number) and that it was not used before.
+    """ 
+
+
+    def __init__(self, word_list, num_lives = 5):
         """ 
-        This method to construct initial class attributes and then additional attributes using these initial attributes:
-        word_list(list): list of words from which a random word is selected.
-        num_lives (int): numbers of lives or attempts that a user has before game ends.
-        word(string): a randomly chosen word from the list of words.
-        word_guessed(list): a list of dashes with each dash representing a letter in the chosen word.
-        num_letters(length): number of unique letters in a chosen word.
-        list_of_guesses(list): a list of correctly guessed letters. 
-        This method picks a random word from the list of words and returns it as a list of empty spaces (dashes). 
+        Constructor method to initialise class attributes.
+        
+        Parameters:
+        ----------
+            self (Hangman): The instance of the Hangman class.
+            word_list (list): List of words for the game. 
+            num_lives (int): Number of attempts allowed. Default is 5.
+
+        Attributes: 
+        ----------
+            self.word (string): Randomly chosen word.
+            self.word_list (list): List of words for the game.
+            self.num_lives (int): Remaining attempts.
+            self.word_guessed (list): List of letters in the word with '_' for unrevealed letters.
+            self.num_letters (length): Number of unique letters in the chosen word.
+            self.list_of_guesses (list): List of correctly guessed letters.              
         """
-        self.word_list = word_list  
-        self.num_lives = num_lives  
-        # attributes defined using other attributes
-        self.word = random.choice(word_list)
-        self.word_guessed = ["_" for letter in self.word]
-        self.num_letters = len(set(self.word)) # unique letters 
-        self.list_of_guesses = [] #empty list
-        print("The mistery word has", len(self.word), "characters")
-        print(self.word_guessed)
-        pass # from template 
- # methods - functions inside the class, what instances of the class can do      
-    def check_guess(self, guess) -> None: #TODO 3 ## checks if the letter inputted is in the hidden word
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list) 
+        self.word_guessed = ["_" for letter in self.word] 
+        self.num_letters = len(set(self.word)) 
+        self.list_of_guesses = []  
+        print(f'\nThe mistery word has {len(self.word)} characters')
+        print(f'\n{self.word_guessed}')
+        
+
+    def check_guess(self, guess) -> None: 
         """
-        This method checks if the letter inputted by the user belongs to the hidden word. 
-        Method's parameter is user's input called guess. 
-        The method also informs the user if the guess was correct.
-         The method also informs how many lives the user has after an incorrect guess.
+        Checks if the guessed letter is in the word and updates the game state accordingly.
+        If the letter is in the word, it replaces the '_' in word_guessed with the letter.
+        If it is not, it decreases the number of lives by 1.
+
+        Parameters:
+        ----------
+            guess (str): Player's guess to be checked.
         """
         guess = str.lower(guess) 
         if guess in self.word:
-            print(f'Good guess! {guess} is in the word.')
-            # new
+            print(f'\nGood guess! {guess} is in the word.')
+            # Update the list of dashes representing a chosen word replacing dashs with a correctly guessed letter.
             for position, letter in enumerate(self.word):
                 if letter == guess:
-                    self.word_guessed[position] = letter #TODO 3
-            self.num_letters -= 1 #TODO 3
+                    self.word_guessed[position] = letter # If the guess is correct, replaces dashes with a correctly guessed letter.
+            self.num_letters -= 1 # Removes the correctly guessed letter from the list of letters to be guessed.
         else:
-            self.num_lives -= 1 #TODO 3
-            print(f'Sorry, {guess} is not in the word')
-            print(f'You have {self.num_lives} lives left.')
-        pass # from hangman.py template
+            self.num_lives -= 1 # If the guess is incorrect, reduces number of lives by 1.
+            print(f'\nSorry, {guess} is not in the word')
+            print(f'\nYou have {self.num_lives} lives left.')
+    
 
     def ask_for_input(self):
+        """ 
+        Asks for player's input, checks if it is a single, alphabetical character (not a number) and that it was not used before.  
         """
-        This method asks for user's input, validates if the input is a single, alphabetical character and not a number.
-        It also checks if the user has already guessed the letter.  
-        """
-        while True: #TODO 1 
-            guess = input('Enter a letter: ') #TODO 1
-            if not (guess.isalpha() and len(guess) == 1): #TODO1 
-                print('Invalid letter. Please, enter a single alphabetical character.')
-            elif guess in self.list_of_guesses: #TODO 2
-                print('You already tried that letter!')
-                print(self.word_guessed) # checking
+        while True:
+            guess = input('Please, enter a letter: ') # Asks the player to input a letter.
+            if not (guess.isalpha() and len(guess) == 1): # Checks if the player's input is single alphabetical (not a number) character.
+                print('\nInvalid letter. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses: # Checks if the player's input has previously been used. 
+                print('\nYou already tried that letter!')
+                print(f'\n{self.word_guessed}')
             else:
-                self.list_of_guesses.append(guess)
-                self.check_guess(guess) #TODO 3
+                self.list_of_guesses.append(guess) # Updates the list of unique letters inputted by the player.
+                self.check_guess(guess) # Checks if the inputted letter is in the chosen word.
+                print('\nCurrent word: ', self.word_guessed) # Print the updated list of dashes for the player to see game progress. 
                 break
-        # pass #from template
+
 
 def play_game(word_list):
     """
-    This method takes word_list as a parameter.
-    It counts how many attempts the user has. Once the remaining attempts equal zero, it informs the user
-    that they lost the game.
-    It also counts if all the unique letters in the word have been guessed and informs the user 
-    if they won.
+    Initialises the game and plays it, counting the number of remaining attempts. 
+    Once the remaining attempts equal zero, it informs the player that they lost the game.
+    It also checks if all the unique letters in the word have been guessed and informs the player if they won.
+
+    Parameters:
+    ----------
+        word_list (list): List of words for the game.
     """
+
     num_lives = 5
-    game = HangMan(word_list, num_lives)
-    while True: #TODO 4
-        if game.num_lives == 0:
-            print(f'You lost! the word was {game.word}')
+    game = Hangman(word_list, num_lives)
+    while True:
+        if game.num_lives == 0: # Once the number of lives reaches zero, print messages that the user lost the game.   
+            print(f'\nYou lost! the word was {game.word}')
             break
-        elif game.num_letters > 0:
+        elif game.num_letters > 0: # Continues the game if a life is still available.
             game.ask_for_input()
-        else: 
-            print('Congratulations. You won the game!')
+        else: # Print message to congratulate the player with winning the game. 
+            print('\nCongratulations. You won the game!')
             break
+
+
 if __name__ == '__main__':
-    """
-    Statement that runs this code only if it runs directly.
-    """
+    # Run this code only if it runs directly. #
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     play_game(word_list)
